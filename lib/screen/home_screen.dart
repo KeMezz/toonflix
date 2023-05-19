714a4cfffdb70d82a440dcd1f6d98b5ae0caf67c
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-05-19 15:01:24 +0900

    create button

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/services/api_service.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+
+  void onPress() {
+    ApiService().getTodaysToons();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        elevation: 2,
+        elevation: 1,
         backgroundColor: Colors.lightGreen,
         title: const Text(
           "오늘의 웹툰",
-          style: TextStyle(
-            fontWeight: FontWeight.w600,
-          ),
+        ),
+      ),
+      body: Center(
+        child: IconButton(
+          icon: const Icon(Icons.add),
+          onPressed: onPress,
         ),
       ),
     );
