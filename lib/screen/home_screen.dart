a7e4f02163a7431055be8f00f701cfcbc9a56d9c
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-05-19 11:35:41 +0900

    add timer function

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +10,19 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int totalSeconds = 1500;
+  late Timer timer;
+
+  void onTick(Timer timer) {
+    setState(() {
+      totalSeconds = totalSeconds - 1;
+    });
+  }
+
+  void onStartPressed() {
+    timer = Timer.periodic(const Duration(seconds: 1), onTick);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +34,7 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Container(
                 alignment: Alignment.bottomCenter,
                 child: Text(
-                  '25:00',
+                  '$totalSeconds',
                   style: TextStyle(
                     color: Theme.of(context).cardColor,
                     fontSize: 80,
@@ -33,7 +48,7 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Center(
                 child: IconButton(
                   iconSize: 140,
-                  onPressed: () {},
+                  onPressed: onStartPressed,
                   icon: const Icon(Icons.play_circle_outline),
                   color: Theme.of(context).cardColor,
                 ),
@@ -47,6 +62,10 @@ class _HomeScreenState extends State<HomeScreen> {
                     child: Container(
                         decoration: BoxDecoration(
                           color: Theme.of(context).cardColor,
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(40),
+                            topRight: Radius.circular(40),
+                          ),
                         ),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
