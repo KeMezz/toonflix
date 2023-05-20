69ef0f283521e453003b0a6636b2794e13620043
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-05-21 00:51:56 +0900

    fetch data using FutureBuilder

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_model.dart';
 import 'package:toonflix/services/api_service.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
 
-  void onPress() {
-    ApiService().getTodaysToons();
-  }
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -18,11 +17,15 @@ class HomeScreen extends StatelessWidget {
           "오늘의 웹툰",
         ),
       ),
-      body: Center(
-        child: IconButton(
-          icon: const Icon(Icons.add),
-          onPressed: onPress,
-        ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const Text("There is data!");
+          } else {
+            return const Text("Loading...");
+          }
+        },
       ),
     );
   }
