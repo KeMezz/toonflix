d2fff33d67eb18cf1b3de6b47b4a11b037598406
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-05-28 13:20:09 +0900

    fetch한 데이터 불러오기
    - DetailScreen을 StatefulWidget으로 변경
    - late와 initState로 페치 데이터 선언

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
+import 'package:toonflix/services/api_service.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
 
   const DetailScreen({
@@ -11,12 +14,27 @@ class DetailScreen extends StatelessWidget {
   });
 
   @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+
+  @override
+  void initState() {
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodesById(widget.id);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           elevation: 1,
           backgroundColor: Colors.lightGreen,
-          title: Text(title),
+          title: Text(widget.title),
         ),
         body: Column(
           children: [
@@ -25,7 +43,7 @@ class DetailScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Hero(
-                  tag: id,
+                  tag: widget.id,
                   child: Container(
                     width: 250,
                     clipBehavior: Clip.hardEdge,
@@ -39,7 +57,7 @@ class DetailScreen extends StatelessWidget {
                         ),
                       ],
                     ),
-                    child: Image.network(thumb),
+                    child: Image.network(widget.thumb),
                   ),
                 ),
               ],
