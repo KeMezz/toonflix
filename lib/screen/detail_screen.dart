9907bd0784e7a38158cdbf5fe5ce4202334f23c6
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-05-28 12:37:53 +0900

    Webtoon 위젯 분리, 디테일 페이지 네비게이션

---
+++ lib/screen/detail_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  final String title, thumb, id;
+
+  const DetailScreen({
+    super.key,
+    required this.title,
+    required this.thumb,
+    required this.id,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 1,
+          backgroundColor: Colors.lightGreen,
+          title: Text(title),
+        ),
+        body: Column(
+          children: [
+            const SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 250,
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 5,
+                        offset: const Offset(0, 0),
+                        color: Colors.black.withOpacity(0.5),
+                      ),
+                    ],
+                  ),
+                  child: Image.network(thumb),
+                ),
+              ],
+            ),
+          ],
+        ));
+  }
+}
