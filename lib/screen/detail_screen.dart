8ba1ee8587d362b3c3f774f9847f63aa8b8cf8de
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-05-28 14:00:23 +0900

    add FutureBuilder to DetailScreen

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -62,6 +62,31 @@ class _DetailScreenState extends State<DetailScreen> {
                 ),
               ],
             ),
+            const SizedBox(height: 24),
+            FutureBuilder(
+              future: webtoon,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          snapshot.data!.about,
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                        const SizedBox(
+                          height: 14,
+                        ),
+                        Text("${snapshot.data!.genre} / ${snapshot.data!.age}"),
+                      ],
+                    ),
+                  );
+                }
+                return const Text("불러오는 중...");
+              },
+            )
           ],
         ));
   }
