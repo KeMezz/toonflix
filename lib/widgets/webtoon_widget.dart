8e86a580ce126cb585916caad4cdc736d130f4e9
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-07-23 17:14:34 +0900

    separate webtoon widget

---
+++ lib/widgets/webtoon_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class WebtoonWidget extends StatelessWidget {
+  const WebtoonWidget({
+    super.key,
+    required this.thumb,
+    required this.title,
+  });
+
+  final String thumb, title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 250,
+          clipBehavior: Clip.hardEdge,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 8,
+                  offset: const Offset(2, 2),
+                  color: Colors.black.withOpacity(0.2),
+                ),
+              ]),
+          child: Image.network(
+            thumb,
+            headers: const {
+              "User-Agent":
+                  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+            },
+          ),
+        ),
+        const SizedBox(
+          height: 14,
+        ),
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ],
+    );
+  }
+}
