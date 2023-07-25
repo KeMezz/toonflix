cc143de82b285433967c153e790ec07a0ddc6a4d
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-07-25 21:23:21 +0900

    stateful에서 property 값을 초기화
    stateless widget에서 property 중 하나인 id 값을 바로 참조하여 API 요청을 보낼 수 없기 때문에(앱 초기화 시점에서는 id에 값이 들어있지 않음), stateful widget으로 변경한 뒤에 late로 선언해두고 initState 메소드 안에서 API 요청할때 id에 접근하여 가져올 수 있도록 함

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/services/api_service.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String id, title, thumb;
 
   const DetailScreen({
@@ -11,12 +13,25 @@ class DetailScreen extends StatelessWidget {
   });
 
   @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoon;
+
+  @override
+  void initState() {
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 2,
-        title: Text(title),
+        title: Text(widget.title),
         backgroundColor: Colors.lightGreenAccent,
         foregroundColor: Colors.black,
         titleTextStyle: const TextStyle(
@@ -45,7 +60,7 @@ class DetailScreen extends StatelessWidget {
                       ),
                     ]),
                 child: Image.network(
-                  thumb,
+                  widget.thumb,
                   headers: const {
                     "User-Agent":
                         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
